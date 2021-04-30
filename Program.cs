74fe4dab849a1f4e8fbd4e12165e5726f99934dc
William <capuletos@live.com>
2021-04-30 20:12:11 -0300

    Starting event testing project

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HotelEvent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Iniciando método de Teste
+
+
+
+        }
+    }
+}
