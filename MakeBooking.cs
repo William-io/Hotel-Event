5c4b06f9ac282502892a0de6b688cd691acfb078
William <capuletos@live.com>
2021-05-03 20:06:16 -0300

    Finalizando Método de Evento

---
+++ MakeBooking.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelEvent
+{
+    public class MakeBooking
+    {
+
+        private int RoomsInUse = 0;
+        public int Rooms { get; set; }
+
+        public MakeBooking(int rooms)
+        {
+            RoomsInUse = 0;
+            Rooms = rooms;
+        }
+
+        public void Bookings()
+        {
+            RoomsInUse++;
+            if (RoomsInUse >= Rooms)
+            {
+                //Evento Fechado! Sala está cheia.
+                OnRoomSoldOut(EventArgs.Empty);
+            }
+            else
+            {
+                Console.WriteLine("Quarto Reservado!");
+            }
+        }
+
+        public event EventHandler RoomSoldOutEvent;
+
+        protected virtual void OnRoomSoldOut(EventArgs test)
+        {
+            EventHandler handler = RoomSoldOutEvent;
+            handler?.Invoke(this, test);
+        }
+
+        public void OnRoomSoldOut(object sender, EventArgs test)
+        {
+            Console.WriteLine("Hotel Lotado!");
+        }
+
+
+
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,7 +7,18 @@ namespace HotelEvent
         static void Main(string[] args)
         {
             //Iniciando método de Teste
+            //Quantos quarto vão está disponiveis no hotel?(3)
 
+            var room = new MakeBooking(2);
+            room.RoomSoldOutEvent += room.OnRoomSoldOut;
+
+            //Número de reservas 
+            room.Bookings();
+            room.Bookings();
+            room.Bookings();
+            room.Bookings();
+            room.Bookings();
+            room.Bookings();
 
 
         }
